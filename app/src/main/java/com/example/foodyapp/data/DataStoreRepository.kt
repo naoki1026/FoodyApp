45d2bc7d8325c45870465f6b5d3ea814fec28b55
Naoki Arakawa <naoki.arakawa1026@gmail.com>
2021-06-02 16:34:40 +0900

    58 Create and Inject DataStoreRepository classまで

---
+++ app/src/main/java/com/example/foodyapp/data/DataStoreRepository.kt
@@ -0,0 +1,74 @@
+package com.example.foodyapp.data
+
+import android.content.Context
+import androidx.datastore.DataStore
+import androidx.datastore.preferences.*
+import com.example.foodyapp.util.Constants.Companion.DEFAULT_DIET_TYPE
+import com.example.foodyapp.util.Constants.Companion.DEFAULT_MEAL_TYPE
+import com.example.foodyapp.util.Constants.Companion.PREFERENCES_DIET_TYPE
+import com.example.foodyapp.util.Constants.Companion.PREFERENCES_DIET_TYPE_ID
+import com.example.foodyapp.util.Constants.Companion.PREFERENCES_MEAL_TYPE
+import com.example.foodyapp.util.Constants.Companion.PREFERENCES_MEAL_TYPE_ID
+import com.example.foodyapp.util.Constants.Companion.PREFERENCES_NAME
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.android.scopes.ActivityRetainedScoped
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
+import java.io.IOException
+import javax.inject.Inject
+
+// クラス自体のインスタンス化、つまりコンストラクタの引数に対して注入できる
+// プライマリコンストラクタの前に@Injectをつけることで引数に対して効果を発揮する
+@ActivityRetainedScoped
+class DataStoreRepository @Inject constructor(@ApplicationContext private val context: Context) {
+    private object PreferenceKeys {
+        val selectedMealType = preferencesKey<String>(PREFERENCES_MEAL_TYPE)
+        val selectedMealTypeId = preferencesKey<Int>(PREFERENCES_MEAL_TYPE_ID)
+        val selectedDietType = preferencesKey<String>(PREFERENCES_DIET_TYPE)
+        val selectedDietTypeId = preferencesKey<Int>(PREFERENCES_DIET_TYPE_ID)
+    }
+
+    // createDataStoreには2種類あるため注意
+    private val dataStore : DataStore<Preferences> = context.createDataStore(
+        name = PREFERENCES_NAME
+    )
+
+    suspend fun saveMealAndDietType(mealType: String, mealTypeId : Int, dietType: String, dietTypeId : Int) {
+        dataStore.edit {  preferences ->
+            preferences[PreferenceKeys.selectedMealType] = mealType
+            preferences[PreferenceKeys.selectedMealTypeId] = mealTypeId
+            preferences[PreferenceKeys.selectedDietType] = dietType
+            preferences[PreferenceKeys.selectedDietTypeId] = dietTypeId
+        }
+    }
+
+    val readMealAndDietType : Flow<MealAndDietType> = dataStore.data
+
+            // 例外処理
+        .catch{ exception ->
+            if (exception is IOException) {
+                emit(emptyPreferences())
+            } else {
+                throw exception
+            }
+        }
+            // mapを使用して、データをViewに表示するための変換を行っている
+        .map { preferences ->
+
+            // 値が登録されていなかった場合は、?:より右側の値となる
+            val selectedMealType = preferences[PreferenceKeys.selectedMealType] ?: DEFAULT_MEAL_TYPE
+            val selectedMealTypeId = preferences[PreferenceKeys.selectedMealTypeId] ?: 0
+            val selectedDietType = preferences[PreferenceKeys.selectedDietType] ?: DEFAULT_DIET_TYPE
+            val selectedDietTypeID = preferences[PreferenceKeys.selectedDietTypeId] ?: 0
+            MealAndDietType(selectedMealType, selectedMealTypeId, selectedDietType, selectedMealTypeId)
+        }
+}
+
+// データクラスを定義
+data class MealAndDietType(
+    val selectedMealType : String,
+    val selectedMealTypeId : Int,
+    val selectedDietType : String,
+    val selectedDietTypeId : Int
+)
--- app/src/main/java/com/example/foodyapp/viewmodels/RecipesViewModel.kt
+++ app/src/main/java/com/example/foodyapp/viewmodels/RecipesViewModel.kt
@@ -4,6 +4,9 @@ import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import com.example.foodyapp.util.Constants
 import com.example.foodyapp.util.Constants.Companion.API_KEY
+import com.example.foodyapp.util.Constants.Companion.DEFAULT_DIET_TYPE
+import com.example.foodyapp.util.Constants.Companion.DEFAULT_MEAL_TYPE
+import com.example.foodyapp.util.Constants.Companion.DEFAULT_RECIPES_NUMBER
 import com.example.foodyapp.util.Constants.Companion.QUERY_ADD_RECIPE_INFORMATION
 import com.example.foodyapp.util.Constants.Companion.QUERY_API_KET
 import com.example.foodyapp.util.Constants.Companion.QUERY_DIET
@@ -14,10 +17,10 @@ import com.example.foodyapp.util.Constants.Companion.QUERY_TYPE
 class RecipesViewModel(application : Application) : AndroidViewModel(application) {
     fun applyQueries(): HashMap<String, String>{
         val queries : HashMap<String, String> = HashMap()
-        queries[QUERY_NUMBER] = "50"
+        queries[QUERY_NUMBER] = DEFAULT_RECIPES_NUMBER
         queries[QUERY_API_KET] = API_KEY
-        queries[QUERY_TYPE] = "main course"
-        queries[QUERY_DIET] = "gluten free"
+        queries[QUERY_TYPE] = DEFAULT_MEAL_TYPE
+        queries[QUERY_DIET] = DEFAULT_DIET_TYPE
         queries[QUERY_ADD_RECIPE_INFORMATION] = "true"
         queries[QUERY_FILL_INGREDIENTS] = "true"
         return queries
